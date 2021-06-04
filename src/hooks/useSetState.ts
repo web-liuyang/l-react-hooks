29933a41179a8a308eedda0351455a8e3b3b7ee3
web-liuyang <984584014@qq.com>
2021-06-04 14:31:41 +0800

    Add: reset func

--- src/hooks/useSetState.ts
+++ src/hooks/useSetState.ts
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
-type ReturnType<T> = [T, (nVal: Partial<T>) => void];
+type ReturnType<T> = [T, (nVal: Partial<T>) => void, () => void];
 
+/** 
+ * 设置State
+ * @param {object} object - 设置的值
+ */
 const useSetState = <T extends object = {}>(obj: T): ReturnType<T> => {
   const [value, setValue] = useState<T>(obj);
 
+  /** 设置 - Value */
   const handleSetValue = (nVal: Partial<T>): void => {
     setValue(oVal => ({ ...oVal, ...nVal }));
   };
 
-  return [value, handleSetValue];
+  /** 重置 - Value */
+  const handleResetValue = () => {
+    setValue(obj);
+  };
+
+  return [value, handleSetValue, handleResetValue];
 };
 
 export default useSetState;
